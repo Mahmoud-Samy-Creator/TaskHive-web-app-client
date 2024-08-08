d927a0eb28442e9bb7e9b70bc0cef7936230affb
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-08-08 03:09:49 +0300

    Login component

---
+++ src/Pages/LoginAndRegisterPages/LoginPage/Login.js
@@ -0,0 +1,35 @@
+import React from "react";
+import SignupLoginRef from "../MainComponents/SignupLoginRef";
+import FormHeader from "../MainComponents/FormHeader";
+import LoginForm from "./LoginComponents/LoginForm";
+import '../style/style.css';
+
+export default function Login() {
+    return(
+        <section className="ftco-section">
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-lg-10">
+						<div className="wrap d-md-flex card">
+							<div className="text-wrap p-4 text-center d-flex align-items-center order-md-last">
+								<SignupLoginRef
+									headerContent="Welcome to login"
+									statment="Don't have an account?"
+									hyperLink="/register"
+									linkContent="Sign Up"
+								/>
+							</div>
+							<div className="login-wrap p-4 p-lg-5">
+								<FormHeader 
+									headerContent="Sign In"
+									rule="login"
+								/>
+								<LoginForm />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+    );
+}
