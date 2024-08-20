b5b26ac88f3803eb681b5700c5859b65aaf8bcfe
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-08-20 20:04:56 +0300

    User Home Component

---
+++ src/Pages/HomePage/Home/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import SideBar from '../SideBar/SideBar';
+import Workspaces from "../Workspaces/Workspaces";
+import AccountSetting from '../AccountSetting/AccountSetting';
+import Calender from "../Calender/Calender";
+import { Route, Routes } from 'react-router-dom';
+
+import './Home.scss';
+
+export default function Home() {
+    return(
+        <div className="home-page">
+            <SideBar />
+            <Routes>
+                <Route path="/" element={<Workspaces />} />
+                <Route path="/workspaces/*" element={<Workspaces />} />
+                <Route path="/account-setting" element={<AccountSetting />} />
+                <Route path="/calender" element={<Calender />} />
+            </Routes>
+            </div>
+    );
+}
