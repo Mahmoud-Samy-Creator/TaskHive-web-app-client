437dc687296924e83e44fb243f3c2ce86a4a3d22
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-08-20 20:18:24 +0300

    Existing Workspaces Navigation

---
+++ src/Pages/HomePage/Workspaces/WorkspacesNav/WorkspacesNav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './WorkspacesNav.scss';
+import CurrentWorkspaces from "./CurrentWorkspaces/CurrentWorkspaces";
+import CurrentMembers from "./CurrentMembers/CurrentMembers";
+
+export default function WorkspacesNav() {
+    return(
+        <div className="workspaces-nav">
+            <CurrentWorkspaces />
+            <CurrentMembers />
+        </div>
+    );
+}
