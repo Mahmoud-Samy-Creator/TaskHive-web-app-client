a321265bfd14c38154930646310bd0dafa622af0
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-08-20 20:20:29 +0300

    Project dashboard

---
+++ src/Pages/HomePage/Workspaces/WorkspacesNav/CurrentWorkspaces/WorkspaceDashBoard/ExsistingProjectDashboard/ProjectDashboard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './ProjectDashboard.scss';
+import DashboardHeader from "./DashboardHeader/DashboardHeader";
+import ProjectProperties from './ProjectProperties/ProjectProperties';
+import KanbanBoard from "./KanbanBoard/KanbanBoard";
+import ProjectOverView from "./ProjectOverView/ProjectOverView";
+import ProjectNotes from "./ProkectNotes/ProkectNotes";
+import ProjectQuestions from "./ProjectQuestions/ProjectQuestions";
+import { useParams, Routes, Route } from "react-router-dom";
+
+export default function ProjectDashboard() {
+    const { workspaceId, projectName } = useParams();
+    console.log(workspaceId);
+    console.log(projectName);
+
+    return (
+        <div className="dashboard">
+            <DashboardHeader name={projectName}/>
+            <ProjectProperties name={projectName}/>
+            <Routes>
+                <Route path={``} element={<ProjectOverView />}/>
+                <Route path={`overview`} element={<ProjectOverView />}/>
+                <Route path={`KanbanBoard`} element={<KanbanBoard />}/>
+                <Route path={`notes`} element={<ProjectNotes />}/>
+                <Route path={`questions`} element={<ProjectQuestions />}/>
+            </Routes>
+        </div>
+    );
+}
