de0502c70698bbbbb006fad3036af073dcd28443
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-08-20 20:17:05 +0300

    Project demands

---
+++ src/Pages/HomePage/Workspaces/UserSummury/WordspacesDemands/ProjectsDemands.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './ProjectsDemands.scss'
+export default function ProjectsDemands() {
+    return(
+        <section className='wordspaces-demands'>
+            <span>Projects Working On</span>
+        </section>
+    );
+}
