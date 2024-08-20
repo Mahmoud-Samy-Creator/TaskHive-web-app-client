44ba3bb6417c58f76e4400411a8c76ec347ad082
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-08-20 20:16:23 +0300

    PLACEHOLDER: User Feeds

---
+++ src/Pages/HomePage/Workspaces/UserSummury/UserSummuryFeeds/UserSummuryFeeds.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './UserSummuryFeeds.scss';
+
+export default function UserSummuryFeeds() {
+    return(
+        <section className='user-incoming-message'>
+            <span>Up coming feeds</span>
+        </section>
+    );
+}
